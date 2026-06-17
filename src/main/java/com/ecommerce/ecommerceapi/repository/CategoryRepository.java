40c86f0090d8d3745657dcfbfde8481710491139
Nguyen Thanh Luan <nluan5517@gmail.com>
2026-06-17 13:49:44 +0700

    chỉnh sửa lại file về import JpaRepository

--- src/main/java/com/ecommerce/ecommerceapi/repository/CategoryRepository.java
+++ src/main/java/com/ecommerce/ecommerceapi/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.ecommerce.ecommerceapi.repository;
 
 import com.ecommerce.ecommerceapi.entity.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CategoryRepository extends CrudRepository<Category, Integer> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
     // Chỉ cần viết trống như thế này thôi!
     // Kế thừa JpaRepository là  nghiễm nhiên có sẵn các hàm:
     // findAll(), findById(), save(), deleteById()... không cần code thêm gì cả.
--- src/main/java/com/ecommerce/ecommerceapi/repository/ProductRepository.java
+++ src/main/java/com/ecommerce/ecommerceapi/repository/ProductRepository.java
@@ -1,12 +1,12 @@
 package com.ecommerce.ecommerceapi.repository;
 
 import com.ecommerce.ecommerceapi.entity.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductRepository  extends CrudRepository<Product,Integer> {
+public interface ProductRepository  extends JpaRepository<Product,Integer> {
     // Ngoài các hàm cơ bản, bro có thể tự định nghĩa thêm hàm tìm kiếm theo category_id cực kỳ nhanh:
     List<Product> findByCategoryId(Integer categoryId);
 }
